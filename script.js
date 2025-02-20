7ae5db1e9067f47f2a469ebacb232b1d25e2da17
BrunoBerwig <contatobrunoberwig@gmail.com>
2025-02-20 15:58:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,13 +8,12 @@ document.addEventListener("click", (e) => {
     }
 })
 
-document.addEventListener('keypress', (e) =>{
-    if(e.code = 'Space'){
-        personagem.velocidadey = -15
-        personagem.pulando = true
-    } 
-   
-})
+document.addEventListener('keypress', (e) => {
+    if (e.code === 'Space' && !personagem.pulando) {
+        personagem.velocidadey = -15; 
+        personagem.pulando = true;
+    }
+});
 
 let gameOver = false;
 
@@ -40,9 +39,15 @@ function atualizaPersonagem(){
         personagem.velocidadey += gravidade 
         personagem.posicaoy += personagem.velocidadey
         personagem.posy += personagem.velocidadey;
+        if(personagem.posicaoy>=canvas.height-50){
+            personagem.velocidadey = 0
+            personagem.pulando=false
+        }
     }
 }
+function teste(){
 
+}
 const obstaculo = {
     posx:canvas.width-100,
     posy:canvas.height-100,
@@ -61,14 +66,18 @@ function desenhaObstaculo(){
     )
 }
 
-function atualizaObstaculo(){
-    obstaculo.posx -= obstaculo.velocidade
+function atualizaObstaculo() {
+    obstaculo.posx -= obstaculo.velocidade;
+    if (obstaculo.posx <= 0 - obstaculo.tamx) {  
+        obstaculo.posx = canvas.width - 100;
+        obstaculo.velocidade += 1;
+    }
 }
 
-function verificaPosicaoChao(){
-    if(personagem.posy >= canvas.height - personagem.altura) {
-        personagem.posy = canvas.height - personagem.altura; 
-        personagem.pulando = false; 
+function verificaPosicaoChao() {
+    if (personagem.posy >= canvas.height - personagem.altura) {
+        personagem.posy = canvas.height - personagem.altura;
+        personagem.pulando = false;
         personagem.velocidadey = 0; 
     }
 }
@@ -86,19 +95,23 @@ function verificaColisao() {
 }
 
 function houveColisao(){
-    obstaculo.velocidade = 0
-    atualizaObstaculo()
-    ctx.fillStyle = 'red'
-    ctx.fillRect((canvas.width/2)-150, (canvas.height/2)-50, 400, 100)
-    ctx.fillStyle = 'black'
-    ctx.font = '50px Arial'
-    ctx.fillText("GAME OVER", (canvas.width/2)-50, (canvas.height/2)+20)
-    gameOver = true
+    obstaculo.velocidade = 0;
+    atualizaObstaculo();
+    ctx.fillStyle = 'red';
+    const largura = 400;
+    const altura = 100;
+    ctx.fillRect((canvas.width / 2) - (largura / 2), (canvas.height / 2) - (altura / 2), largura, altura);
+    ctx.fillStyle = 'black';
+    ctx.font = '50px Arial';
+
+    const texto = "GAME OVER";
+    const textoWidth = ctx.measureText(texto).width;
+    ctx.fillText(texto, (canvas.width / 2) - (textoWidth / 2), (canvas.height / 2) + 20);
+    gameOver = true;    
 }
 
 function loop(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    verificaColisao
     desenhaObstaculo()
     atualizaPersonagem()
     verificaPosicaoChao()
